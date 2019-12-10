456bf235bb4b3e10573d4fed4f876f7621ce6a16
Roman <vrv2323@gmail.com>
2019-12-10 16:06:34 +0200

    Implemented Controller
    Add endpoint GetGitHubRepositories

---
+++ GitWatch.Api/Controllers/ServiceController.cs
@@ -0,0 +1,35 @@
+﻿using GitWatch.Data.Auth;
+using GitWatch.Data.Services;
+using GitWatch.Domain.Models;
+using GitWatch.Domain.Repositories;
+using GitWatch.Domain.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitWatch.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/auth")]
+    public class ServiceController : Controller
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IProjectService _projectService;
+
+        public ServiceController(ProjectRepository projectRepository, ProjectService projectService)
+        {
+            _projectRepository = projectRepository;
+            _projectService = projectService;
+        }
+
+        /// <summary>
+        /// Repository List
+        /// </summary>
+        [HttpGet("api/repository_from_git_hub")]
+        public async Task<IActionResult> GetGitHubRepositories(string login, DateTime startDate = new DateTime()) =>
+            new OkObjectResult(await _projectService.ProjectPages(login, startDate));
+    }
+}
