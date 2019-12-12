a3b02e2aea975bf57733eb5dbe924a43c95a8038
Roman <vrv2323@gmail.com>
2019-12-12 16:36:16 +0200

    Core - add local exception

---
+++ GitWatch.Core/ExceptionError/ExceptionTerm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitWatch.Core.ExceptionError
+{
+    public enum ExceptionTerm
+    {
+        UserNotFoundFailure
+    }
+}
---
+++ GitWatch.Core/ExceptionError/ExceptionTermFailure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitWatch.Core.ExceptionError
+{
+    public abstract class ExceptionTermFailure : Exception
+    {
+        private ExceptionTerm ExceptionTerm { get; }
+        protected ExceptionTermFailure(ExceptionTerm exceptionTerm)
+        {
+            ExceptionTerm = exceptionTerm;
+        }
+    }
+}
